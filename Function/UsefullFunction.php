7cee22c61224cd796b918889091fdcd1d31358f8
Jea杨 <jjonline@jjonline.cn>
2015-08-18 22:09:06 +0800

    redirect、send_http_status、is_ssl、get_client_ip、dump

--- Function/UsefullFunction.php
+++ Function/UsefullFunction.php
@@ -402,6 +402,221 @@ namespace {
 	}
 
 	/**
+	 * 判断是否SSL协议
+	 * @return boolean
+	 */
+	function is_ssl() {
+	    if(isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))){
+	        return true;
+	    }elseif(isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'] )) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/**
+	 * 获取客户端IP地址
+	 * @param mixed $type 返回类型 0|false 返回IP地址 1|true 返回IPV4地址数字
+	 * @param boolean $adv 是否进行高级模式获取（有可能被伪装） 
+	 * @return mixed
+	 */
+	function get_client_ip($type = 0,$adv=false) {
+	    $type       =  $type ? 1 : 0;
+	    static $ip  =   NULL;
+	    if ($ip !== NULL) return $ip[$type];
+	    if($adv){
+	        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+	            $pos    =   array_search('unknown',$arr);
+	            if(false !== $pos) unset($arr[$pos]);
+	            $ip     =   trim($arr[0]);
+	        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+	            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+	        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	            $ip     =   $_SERVER['REMOTE_ADDR'];
+	        }
+	    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	        $ip     =   $_SERVER['REMOTE_ADDR'];
+	    }
+	    // IP地址合法验证
+	    $long = sprintf("%u",ip2long($ip));
+	    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+	    return $ip[$type];
+	}
+
+	/**
+	 * URL重定向 重定向后调用该函数的脚本将终止运行
+	 * @param string $url 重定向的URL地址
+	 * @param integer $time 重定向的等待时间（秒）
+	 * @param string $msg 重定向前的提示信息
+	 * @return void
+	 */
+	function redirect($url, $time=0, $msg='') {
+	    //多行URL地址支持
+	    $url        = str_replace(array("\n", "\r"), '', $url);
+	    if (empty($msg))
+	        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+	    if (!headers_sent()) {
+	        // redirect
+	        if (0 === $time) {
+	            header('Location: ' . $url);
+	        } else {
+	            header("refresh:{$time};url={$url}");
+	            echo($msg);
+	        }
+	        exit();
+	    } else {
+	        $str      = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+	        if ($time != 0)
+	            $str .= $msg;
+	        exit($str);
+	    }
+	}
+
+	/**
+	 * 浏览器友好的变量输出==用于调试
+	 * @param mixed $var 变量
+	 * @param boolean $echo 是否输出 默认为True 如果为false 则返回输出字符串
+	 * @param string $label 标签 默认为空
+	 * @param boolean $strict 是否严谨 默认为true
+	 * @return void|string
+	 */
+	function dump($var, $echo=true, $label=null, $strict=true) {
+	    $label = ($label === null) ? '' : rtrim($label) . ' ';
+	    if (!$strict) {
+	        if (ini_get('html_errors')) {
+	            $output = print_r($var, true);
+	            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+	        } else {
+	            $output = $label . print_r($var, true);
+	        }
+	    } else {
+	        ob_start();
+	        var_dump($var);
+	        $output = ob_get_clean();
+	        if (!extension_loaded('xdebug')) {
+	            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+	            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+	        }
+	    }
+	    if ($echo) {
+	        echo($output);
+	        return null;
+	    }else
+	        return $output;
+	}
+
+	/**
+	 * 发送HTTP状态
+	 * @param integer $code 状态码
+	 * @return void
+	 */
+	function send_http_status($code) {
+	    static $_status = array(
+	            // Informational 1xx
+	            100 => 'Continue',
+	            101 => 'Switching Protocols',
+	            // Success 2xx
+	            200 => 'OK',
+	            201 => 'Created',
+	            202 => 'Accepted',
+	            203 => 'Non-Authoritative Information',
+	            204 => 'No Content',
+	            205 => 'Reset Content',
+	            206 => 'Partial Content',
+	            // Redirection 3xx
+	            300 => 'Multiple Choices',
+	            301 => 'Moved Permanently',
+	            302 => 'Moved Temporarily ',  // 1.1
+	            303 => 'See Other',
+	            304 => 'Not Modified',
+	            305 => 'Use Proxy',
+	            // 306 is deprecated but reserved
+	            307 => 'Temporary Redirect',
+	            // Client Error 4xx
+	            400 => 'Bad Request',
+	            401 => 'Unauthorized',
+	            402 => 'Payment Required',
+	            403 => 'Forbidden',
+	            404 => 'Not Found',
+	            405 => 'Method Not Allowed',
+	            406 => 'Not Acceptable',
+	            407 => 'Proxy Authentication Required',
+	            408 => 'Request Timeout',
+	            409 => 'Conflict',
+	            410 => 'Gone',
+	            411 => 'Length Required',
+	            412 => 'Precondition Failed',
+	            413 => 'Request Entity Too Large',
+	            414 => 'Request-URI Too Long',
+	            415 => 'Unsupported Media Type',
+	            416 => 'Requested Range Not Satisfiable',
+	            417 => 'Expectation Failed',
+	            // Server Error 5xx
+	            500 => 'Internal Server Error',
+	            501 => 'Not Implemented',
+	            502 => 'Bad Gateway',
+	            503 => 'Service Unavailable',
+	            504 => 'Gateway Timeout',
+	            505 => 'HTTP Version Not Supported',
+	            509 => 'Bandwidth Limit Exceeded'
+	    );
+	    if(isset($_status[$code])) {
+	        header('HTTP/1.1 '.$code.' '.$_status[$code]);
+	        // 确保FastCGI模式下正常
+	        header('Status:'.$code.' '.$_status[$code]);
+	    }
+	}
+
+	/**
+	 * XML编码
+	 * @param mixed $data 数据
+	 * @param string $root 根节点名
+	 * @param string $item 数字索引的子节点名
+	 * @param string $attr 根节点属性
+	 * @param string $id   数字索引子节点key转换的属性名
+	 * @param string $encoding 数据编码
+	 * @return string
+	 */
+	function xml_encode($data, $root='root', $item='item', $attr='', $id='id', $encoding='utf-8') {
+	    if(is_array($attr)){
+	        $_attr = array();
+	        foreach ($attr as $key => $value) {
+	            $_attr[] = "{$key}=\"{$value}\"";
+	        }
+	        $attr = implode(' ', $_attr);
+	    }
+	    $attr   = trim($attr);
+	    $attr   = empty($attr) ? '' : " {$attr}";
+	    $xml    = "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>";
+	    $xml   .= "<{$root}{$attr}>";
+	    $xml   .= data_to_xml($data, $item, $id);
+	    $xml   .= "</{$root}>";
+	    return $xml;
+	}
+
+	/**
+	 * 数据XML编码
+	 * @param mixed  $data 数据
+	 * @param string $item 数字索引时的节点名称
+	 * @param string $id   数字索引key转换为的属性名
+	 * @return string
+	 */
+	function data_to_xml($data, $item='item', $id='id') {
+	    $xml = $attr = '';
+	    foreach ($data as $key => $val) {
+	        if(is_numeric($key)){
+	            $id && $attr = " {$id}=\"{$key}\"";
+	            $key  = $item;
+	        }
+	        $xml    .=  "<{$key}{$attr}>";
+	        $xml    .=  (is_array($val) || is_object($val)) ? data_to_xml($val, $item, $id) : $val;
+	        $xml    .=  "</{$key}>";
+	    }
+	    return $xml;
+	}
+
+	/**
 	* 格式化字节大小
 	* @param  number $size      字节数
 	* @param  string $delimiter 数字和单位分隔符
