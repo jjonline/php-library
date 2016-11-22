d0dd5b86f85ffbe4dbd119d1dbcfced6f38d0534
Jea杨 <jjonline@jjonline.cn>
2016-11-22 21:04:16 +0800

    增加nl2br的类似函数nl2p，即将换行转换成p标签

--- Function/UsefullFunction.php
+++ Function/UsefullFunction.php
@@ -8,6 +8,17 @@
 namespace {
 
     /**
+     * nl2br的类似函数，将(多个)换行替换成p标签
+     * @param  string $str
+     * @return string
+     */
+    function nl2p($str)
+    {
+        $str = str_replace(array('<p>', '</p>', '<br>', '<br/>', '<br />'), '', $str);
+        return '<p>'.preg_replace("/([\n|\r\n|\r]{1,})/i", "</p>\n<p>", trim($str)).'</p>';
+    }
+
+    /**
      * 获取输入参数 支持过滤和默认值 author:Tp
      * 使用方法:
      * <code>
